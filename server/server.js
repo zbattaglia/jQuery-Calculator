2c750a72f81cd7c1aba9e1770e298fe713d9bbab
zbattaglia <zbattaglia3@gmail.com>
2020-03-27 11:45:26 -0500

    added process.env.PORT

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use( bodyParser.urlencoded ({ extended:true } ));
 app.use( express.static( 'server/public'));
